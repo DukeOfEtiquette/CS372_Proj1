2c745a135f6f3ec2b1b11576ad35ac7a90a8fbd2
Adam DuQuette <duquetteadam@gmail.com>
2016-10-30 20:45:42 -0700

    Created client socket, tested and works

--- client.cpp
+++ client.cpp
@@ -6,12 +6,30 @@
 #include <sys/types.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <netdb.h>
 
+#define PORT_NUM 9999
 
 int main(int argc, char **argv)
 {
+    struct sockaddr_in server_addr;
+    std::string ip =  "127.0.0.1";
 
-    std::cout << "Hello world\n";
+    //Create the client socket
+    int client = socket(AF_INET, SOCK_STREAM, 0);
+
+    //Verify socket was created, print message to std with result
+    if(client < 0)
+    {
+        std::cout << "Error encountered while creating the client socket.\n";
+        return 1;
+    }else
+    {
+        std::cout << "Client socket created successfully.\n";
+    }
+
+
+    close(client);
 
     return 0;
 }
