1becd383f88b1fb5f7bd051ad5cc630dbbaecaed
Adam DuQuette <duquetteadam@gmail.com>
2016-10-30 20:30:53 -0700

    Added needed headers

--- client.cpp
+++ client.cpp
@@ -1,4 +1,12 @@
 #include <iostream>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 
 int main(int argc, char **argv)
 {
